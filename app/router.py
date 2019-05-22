9198f9a1e345f471e12e587f04f3281ff86f9c90
Célio Barros <celio.barros.filho@gmail.com>
2019-05-22 19:24:30 -0300

    Change router

--- app/router.py
+++ app/router.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import jsonify
+from auxiliar_function import number_to_word
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<number>")
+def hello(number):
+  return jsonify({"extenso": number_to_word(int(number)) })
 
 if __name__ == '__main__':
-	app.run(port=3000)
+	app.run(port=3000, use_reloader=True)
