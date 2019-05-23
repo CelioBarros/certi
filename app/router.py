9fa32e95c03030618e4a8a27c09d6277b32a3caf
Célio Barros <celio.barros.filho@gmail.com>
2019-05-23 19:35:57 -0300

    Return bad request

--- app/router.py
+++ app/router.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/<number>")
 def hello(number):
-  return jsonify({"extenso": number_to_word(int(number)) })
+  try:
+    number = int(number)
+    return jsonify({"extenso": number_to_word(number) })
+  except:
+    return 'bad request!', 400
 
 if __name__ == '__main__':
 	app.run(port=3000, use_reloader=True)
