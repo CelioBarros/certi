4a2ce57d766c855a51fb04d035543f9890f23338
Célio Barros <celio.barros.filho@gmail.com>
2019-05-28 18:48:46 -0300

    Remove spaces

--- app/router.py
+++ app/router.py
@@ -12,4 +12,4 @@ def hello(number):
     return jsonify({"error": 'bad request!' }), 400
 
 if __name__ == '__main__':
-	app.run(port=3000, use_reloader=True, host='0.0.0.0')
+	app.run(port=3000,use_reloader=True,host='0.0.0.0')
